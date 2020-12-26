4023431eb8497328866c622a6f0f90a38d49459f
Exzender <42044185+Exzender@users.noreply.github.com>
2020-12-26 16:25:23 +0300

    Update vs.sh
    link changed

--- vs.sh
+++ vs.sh
@@ -2,12 +2,12 @@ cd /tmp
 if [ -f "img" ]; then
     rm -rf img
 fi
-wget http://download.hiveos.farm/asic/s9/Antminer-S9-all-Hiveon.tar.gz
-tar -xzf Antminer-S9-all-Hiveon.tar.gz
+wget https://my.anthill.farm/static/firmwares/standalone/Antminer-S9-xilinx-vnish-3.8.6-anthill.tar.gz
+tar -xzf Antminer-S9-xilinx-vnish-3.8.6-anthill.tar.gz
 rm u.tar.gz
 sh runme.sh
 reboot
 
-#Antminer|sleep 1 && cd /tmp && wget -q http://download.hiveos.farm/asic/repo/s9/hh.sh -O hh.sh && chmod +x hh.sh && sh hh.sh
+#Antminer|sleep 1 && cd /tmp && wget -q https://github.com/Exzender/s9_downloader/raw/main/vs.sh -O vs.sh && chmod +x vs.sh && sh vs.sh
 
 
